0b9b80ebbafc47cff32f50f933cc7936342bca76
Bruno Strandberg <bruno.strandberg@gmail.com>
2024-05-15 14:54:32 +0300

    Included some noise

--- inc/Drone.h
+++ inc/Drone.h
@@ -1,28 +1,33 @@
 #ifndef DRONE_H
 #define DRONE_H
 #include <Eigen/Dense>
+#include <opencv2/opencv.hpp>
+#include <random>
 
 class Drone {
 
 public:
   Drone();
-  void setDest(double X_dest, double Y_dest) { m_dest << X_dest, Y_dest; }
+  void setDest(double X_dest, double Y_dest);
   void flyToDest(double destTolerance);
+  void setAccStd(double acc_std);
+  void setPosStd(double pos_std);
   
   
  private:
   // constants used to simulate the control rool
   static constexpr double m_dt = 0.1;                       // control-loop time interval
   static constexpr double m_acc_max = 1.0;                  // drone max acceleration in m/s2
-  static constexpr double m_acc_var = 1e-5;                  // variance of acceleration reading from IMU
-  static constexpr double m_pos_var = 1e-5;                  // variance of pos reading from GPS
   static constexpr double m_approach_zone = 30;             // target approach speed if within this meters of the destination
   static constexpr double m_speed_tolerance = 1.0;          // don't accelerate if speed is within speed_tolerance of target speed
   static constexpr double m_dir_tolerance = 0.9848;         // don't accelerate if cos(direction) within dir_tolerance
   static constexpr double m_cruise_speed = 10;              // drone cruise speed in m/s
   static constexpr double m_approach_speed = 2;             // drone destination approach speed in m/s
-  
+
+  double m_expected_acc_std;               // std of acceleration reading from IMU as expected by the Kalman process
+  double m_expected_pos_std;               // std of pos reading from GPS as expected by the Kalman process
   double m_current_time;
+  double m_max_flight_time;
   Eigen::MatrixXd m_true_next_state;       // matrix for storing the true state for exploring true vs model
   Eigen::Vector2d m_true_a;                // vector for storing true acceleration values based on which IMU can simulate a reading
   Eigen::Vector2d m_dest;                  // destination XY
@@ -37,10 +42,20 @@ public:
   Eigen::Matrix4d m_Q;                     // control input covariance
   Eigen::Matrix<double, 2, 4> m_H;         // state-to-measurement transition
   Eigen::Matrix2d m_R;                     // position measurement covariance
+
+  std::normal_distribution<double> m_acc_gaus;  // gaussian to simulate random noise in IMU
+  std::normal_distribution<double> m_pos_gaus;  // gaussian to simulate random noise in GPS
+  std::default_random_engine m_generator;       // random number generator
+  
+  cv::Mat m_map;                           // illustration of the flight
   
   void UpdateAcceleration();
   void PredictNextState();
   void EstimateThisState();
+  void UpdateMap();
+  void setQvals(double acc_std);
+  void setRvals(double pos_std);
+  
   
   
 };
--- src/Drone.cpp
+++ src/Drone.cpp
@@ -11,13 +11,18 @@
 #endif
 
 Drone::Drone() :
+  m_expected_acc_std(0.1),
+  m_expected_pos_std(3.0),
+  m_current_time(0),
+  m_max_flight_time(0),
   m_true_next_state(4, 1),
   m_true_a(0.0, 0.0),
   m_dest(0.0, 0.0),
   m_model_state(4, 1),
   m_model_state_cov(4, 4),
   m_model_next_state(4, 1),
-  m_model_next_state_cov(4, 4) {
+  m_model_next_state_cov(4, 4),
+  m_generator(21453) {
 
   // set state's to 0s
   m_model_state << 0.0, 0.0, 0.0, 0.0;
@@ -38,29 +43,98 @@ Drone::Drone() :
   m_B(3, 0) =             0;   m_B(3, 1) =          m_dt;
 
 
-  // convariance matrix for IMU input, assuming same variance of a along x and y
-  m_Q.setZero();
-  double _t1 = pow(m_dt, 4)*m_acc_var/4.0;
-  double _t2 = pow(m_dt, 3) * m_acc_var/2.0;
-  double _t3 = m_dt*m_dt * m_acc_var;
-  m_Q(0, 0) = _t1; m_Q(0, 1) = 0.0; m_Q(0, 2) = _t2; m_Q(0, 3) = 0.0;
-  m_Q(1, 0) = 0.0; m_Q(1, 1) = _t1; m_Q(1, 2) = 0.0; m_Q(1, 3) = _t2;
-  m_Q(2, 0) = _t2; m_Q(2, 1) = 0.0; m_Q(2, 2) = _t3; m_Q(2, 3) = 0.0;
-  m_Q(3, 0) = 0.0; m_Q(3, 1) = _t2; m_Q(3, 2) = 0.0; m_Q(3, 3) = _t3;
+  // covariance matrices related to measurement from IMU, GPS
+  setQvals(m_expected_acc_std);
+  setRvals(m_expected_pos_std);
   
   // state-to-measurement matrix
   m_H.setZero();
   m_H(0, 0) = 1.0;
   m_H(1, 1) = 1.0;
 
-  // position measurement covariance
-  m_R.setZero();
-  m_R(0, 0) = m_pos_var;
-  m_R(1, 1) = m_pos_var;
+  // distributions for simulating noise
+  m_acc_gaus = std::normal_distribution<double>(0.0, m_expected_acc_std);
+  m_pos_gaus = std::normal_distribution<double>(0.0, m_expected_pos_std);
+  
+  // image to draw positions
+  m_map = cv::Mat(500, 500, CV_8UC3, cv::Scalar(0, 0, 0));
 }
 
+// -----------------------------------------------------------------------------
+
 /** 
-    @brief                Function that performs drone control logic to command acceleration
+    @brief Function to set control input covariance matrix parameters depending on the expected accelerometer std
+*/
+void Drone::setQvals(double acc_std) {
+  m_Q.setZero();
+  double _t1 = pow(m_dt, 4)*acc_std*acc_std/4.0;
+  double _t2 = pow(m_dt, 3) * acc_std*acc_std/2.0;
+  double _t3 = m_dt*m_dt * acc_std*acc_std;
+  m_Q(0, 0) = _t1; m_Q(0, 1) = 0.0; m_Q(0, 2) = _t2; m_Q(0, 3) = 0.0;
+  m_Q(1, 0) = 0.0; m_Q(1, 1) = _t1; m_Q(1, 2) = 0.0; m_Q(1, 3) = _t2;
+  m_Q(2, 0) = _t2; m_Q(2, 1) = 0.0; m_Q(2, 2) = _t3; m_Q(2, 3) = 0.0;
+  m_Q(3, 0) = 0.0; m_Q(3, 1) = _t2; m_Q(3, 2) = 0.0; m_Q(3, 3) = _t3;
+}
+
+// -----------------------------------------------------------------------------
+
+/** 
+    @brief Function to set position measurement covariance matrix parameters depending on the expected position variance
+*/
+void Drone::setRvals(double pos_std) {
+  m_R.setZero();
+  m_R(0, 0) = pos_std*pos_std;
+  m_R(1, 1) = pos_std*pos_std;
+}
+
+// -----------------------------------------------------------------------------
+
+/** 
+    @brief  Set the variance for simulating noise in the accelerometer reading
+
+    This does not affect the expected variance in the Kalman process, the purpose is to inspect
+    what happens if the Kalman process assumes a different variance than actually inherent to the IMU
+
+    @param  acc_std     Accelerometer standard dev
+*/
+void Drone::setAccStd(double acc_std) {
+  m_acc_gaus = std::normal_distribution<double>(0.0, acc_std);  
+}
+
+// -----------------------------------------------------------------------------
+
+/** 
+    @brief  Set the variance for simulating noise in the GPS reading
+
+    This does not affect the expected variance in the Kalman process, the purpose is to inspect
+    what happens if the Kalman process assumes a different variance than actually inherent to the GPS
+
+    @param  pos_std     Position standard dev
+*/
+void Drone::setPosStd(double pos_std) {
+  m_pos_gaus = std::normal_distribution<double>(0.0, pos_std);  
+}
+
+// -----------------------------------------------------------------------------
+
+/**
+   @brief Function to set destination for the drone
+   @param X_dest    X coordinate
+   @param Y_dest    Y coordinate
+ */
+void Drone::setDest(double X_dest, double Y_dest) {
+  m_dest << X_dest, Y_dest;
+  Eigen::Vector2d pos_est(m_model_state(0, 0), m_model_state(1, 0));
+  auto vec_to_dest = m_dest - pos_est;
+  m_max_flight_time = m_current_time + vec_to_dest.norm()/m_cruise_speed*100;
+  std::cout << "Drone::setDest() set max flight time to: " << m_max_flight_time << " sec" << std::endl;
+}
+
+
+// -----------------------------------------------------------------------------
+
+/** 
+    @brief Function that performs drone control logic to command acceleration
 */
 
 void Drone::UpdateAcceleration() {
@@ -99,8 +173,15 @@ void Drone::UpdateAcceleration() {
   
 }
 
+// -----------------------------------------------------------------------------
+
+
+/**
+   @ brief Function to predict the next state       
+ */
 void Drone::PredictNextState() {
-  auto acc_imu = m_true_a;  // introduce noise here
+  Eigen::Vector2d acc_imu(m_true_a(0) + m_acc_gaus(m_generator), m_true_a(1) + m_acc_gaus(m_generator));
+  DEBUG_MSG("True a: \n" << m_true_a << "\nSim a: \n" << acc_imu);
   m_true_next_state = m_A * m_true_next_state + m_B * m_true_a;
   m_model_next_state = m_A * m_model_state + m_B * acc_imu;
   m_model_next_state_cov = m_A * m_model_state_cov * m_A.transpose() + m_Q;
@@ -111,19 +192,24 @@ void Drone::PredictNextState() {
   DEBUG_MSG("--------------------------------------------------------");
 }
 
+// -----------------------------------------------------------------------------
+
 /** 
-    @brief                Function that estimates the current state
+    @brief Function that estimates the current state
 */
 void Drone::EstimateThisState() {
   Eigen::MatrixXd true_pos(2, 1);
-  true_pos(0, 0) = m_true_next_state(0, 0); true_pos(1, 0) = m_true_next_state(1, 0);
+  true_pos(0, 0) = m_true_next_state(0, 0) + m_pos_gaus(m_generator);
+  true_pos(1, 0) = m_true_next_state(1, 0) + m_pos_gaus(m_generator);
   auto kalman_gain = m_model_next_state_cov * m_H.transpose()*(m_H * m_model_next_state_cov * m_H.transpose() + m_R).inverse();
-  std::cout << m_model_next_state << " <-- predicted next state" << std::endl;
-  std::cout << true_pos - m_H * m_model_next_state << " <-- distance delta" << std::endl;
-  std::cout << kalman_gain << " <-- kalman gain" << std::endl;
+  DEBUG_MSG(m_model_next_state << " <-- predicted next state");
+  DEBUG_MSG(true_pos - m_H * m_model_next_state << " <-- distance delta");
+  DEBUG_MSG(kalman_gain << " <-- kalman gain");
   m_model_state = m_model_next_state + kalman_gain * (true_pos - m_H * m_model_next_state);
   m_model_state_cov = m_model_next_state_cov - kalman_gain * m_H * m_model_next_state_cov;  
 }
+
+// -----------------------------------------------------------------------------
 
 /** 
     @brief                Function that runs a loop that simulates drone flight from current position to destination
@@ -139,7 +225,7 @@ void Drone::flyToDest(double destTolerance) {
     auto distance = (pos_est - m_dest).norm();
     
     if (distance < destTolerance) {
-      std::cout << "The drone postion (" << pos_est(0) << ", " << pos_est(1) << ") is within "
+      std::cout << "NOTICE Drone::flyToDest The drone postion (" << pos_est(0) << ", " << pos_est(1) << ") is within "
                 << destTolerance << " meters of the destination (" << m_dest(0) << ", " << m_dest(1) << "), exiting" << std::endl;
       return;
     }
@@ -148,6 +234,16 @@ void Drone::flyToDest(double destTolerance) {
     PredictNextState();
     m_current_time += m_dt;   // here dt passes
     EstimateThisState();
+    UpdateMap();
+    
+    if (m_current_time >= m_max_flight_time) {
+      std::cout << "WARNING Drone::flyToDest drone battery dying, mission failed" << std::endl;
+      return;
+    }
   }
   // code that executes a while loop that ends when the drone thinks it has reached it's destination
 }
+
+// -----------------------------------------------------------------------------
+
+void Drone::UpdateMap() {}
